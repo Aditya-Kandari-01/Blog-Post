af6f2d20f35758a46f4701387756794455adf690
Aditya-Kandari-01 <adityakandari2018@gmail.com>
2025-08-07 18:56:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,57 +1,47 @@
-require('dotenv').config();
-
+require ('dotenv').config();
 const express = require('express');
+const session = require('express-session');
 const expressLayout = require('express-ejs-layouts');
 const methodOverride = require('method-override');
-const connectionDB = require('./server/config/db');
-const cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser')
 const MongoStore = require('connect-mongo');
-const session = require('express-session');
-
-const { isActiveRoute } = require('./server/helpers/routeHelpers');
-
+const connectDB = require('./server/config/db');
+const {isActiveRoute} = require('./server/helpers/routeHelpers');
 const app = express();
-const PORT = process.env.PORT || 5000; 
- 
 
-connectionDB();
-app.use((req, res, next) => {
-    res.locals.currentRoute = req.originalUrl; // Set currentRoute to the current URL path
-    next();
-});
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 10000
+//Mongo Db connection
+connectDB();
+
+//MiddleWare
+app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));//using _method in .ejs files as to convert the post to put request
+
 
 app.use(session({
-    secret: 'dog barks',
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-        mongoUrl: process.env.mongoose_uri,
-    }),
+    secret:'fast tiger',
+    resave:false,
+    saveUninitialized:true,
+    store:MongoStore.create({
+        mongoUrl:process.env.MONGODB_URI
+    })
+}))
 
-}));
 
 app.use(express.static('public'));
 
-
-//Templating Engine
+//To use ejs layout
 app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set('layout','./layouts/main');
+app.set('view engine','ejs');
 
 app.locals.isActiveRoute = isActiveRoute;
 
+app.use('/',require('./server/routes/main'));
+app.use('/',require('./server/routes/register'));
 
-
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'));
-
-
-
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`);
+app.listen(PORT,()=>{
+    console.log(`App listening on ${PORT}`);
 })

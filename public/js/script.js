691e4e3d6b25df7ea75ffadc8ed1c7dfa035989d
Aditya-Kandari-01 <adityakandari2018@gmail.com>
2025-08-07 21:40:29 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,23 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
     const allButtons = document.querySelectorAll('.searchBtn');
     const searchBar = document.querySelector('.searchBar');
     const searchInput = document.getElementById('searchInput');
     const searchClose = document.getElementById('searchClose');
-    for ( var i = 0 ; i < allButtons.length ; i++){
-        allButtons[i].addEventListener('click',function(){
-            searchBar.style.visibility = 'visible';
-            searchBar.classList.add('open');
-            this.setAttribute('aria-expanded','true');
-            searchInput.focus();
-        })
+    if (searchBar && searchInput) {
+        for (var i = 0; i < allButtons.length; i++) {
+            allButtons[i].addEventListener('click', function () {
+                searchBar.style.visibility = 'visible';
+                searchBar.classList.add('open');
+                this.setAttribute('aria-expanded', 'true');
+                searchInput.focus();
+            });
+        }
     }
-        searchClose.addEventListener('click',function(){
+    if (searchClose && searchBar) {
+        searchClose.addEventListener('click', function () {
             searchBar.style.visibility = 'hidden';
             searchBar.classList.remove('open');
-            this.setAttribute('aria-expanded','false');
-        })
-})
+            this.setAttribute('aria-expanded', 'false');
+        });
+    }
+});
